8d764b8f14bd763596c34fcd195ef0150d65216e
parthmern <parthmern@gmail.com>
2024-11-10 17:52:46 -0500

    test: cases added

--- src/routes/api/v1/submissionRoutes.js
+++ src/routes/api/v1/submissionRoutes.js
@@ -1,7 +1,8 @@
-const { createSubmission } = require("../../../controllers/submissionController.js");
+const { createSubmission, pingRequest } = require("../../../controllers/submissionController.js");
 
 async function submissionRoutes(fastify, options) {
     fastify.post('/', createSubmission);
+    fastify.get('/ping', pingRequest);
 }
 
 module.exports = submissionRoutes;
---
+++ tests/ping.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const app = require('../src/app');
+const connectToDB = require('../src/config/dbConfig');
+const evaluationWorker = require('../src/workers/evaluationWorker');
+const fastify = require('fastify')({ logger: false });
+
+jest.mock('../src/config/dbConfig', () => jest.fn(() => Promise.resolve()));
+jest.mock('../src/workers/evaluationWorker', () => jest.fn());
+
+fastify.listen = jest.fn().mockResolvedValue();
+
+beforeAll(async () => {
+    fastify.register(app);
+    await fastify.ready(); 
+});
+
+afterAll(async () => {
+    await fastify.close(); 
+});
+
+describe('Integration tests for Fastify server', () => {
+    it('should respond with a JSON message on GET /ping', async () => {
+        const response = await request(fastify.server).get('/api/v1/submission/ping');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.message).toContain('Submission Service is alive 💚');
+    });
+
+    it('should call connectToDB and evaluationWorker', async () => {
+        // call func
+        await connectToDB();
+        evaluationWorker('EvaluationQueue');
+
+        // ensuring called or not
+        expect(connectToDB).toHaveBeenCalled();
+        expect(evaluationWorker).toHaveBeenCalledWith('EvaluationQueue');
+    });
+});
