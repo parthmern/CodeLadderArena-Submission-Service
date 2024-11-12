9e7ba9dbcb17fa56c1357bda0fb7067da486f198
parthmern <parthmern@gmail.com>
2024-11-12 14:51:43 -0500

    master

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const fastifyPlugin = require('fastify-plugin');
 const servicePlugin = require('./services/servicePlugin');
 const repositoryPlugin = require('./repositories/repositoryPlugin');
+const SubmissionRepository = require('./repositories/submissionRepository');
 
-async function app(fastify,option) {
-    
+async function app(fastify, option) {
+
     await fastify.register(require('@fastify/cors'));
-    
+
     await fastify.register(repositoryPlugin);
 
     await fastify.register(servicePlugin);
@@ -14,7 +15,45 @@ async function app(fastify,option) {
         return { status: 'alive' };
     });
 
-    await fastify.register( require("./routes/api/apiRoutes"), {prefix : '/api'} );
+    fastify.post('/evaluted', async (request, reply) => {
+        try {
+            const payload = request.body;
+
+            console.log('Received payload:', payload);
+
+            console.log("Job data:", payload);
+
+            // Updating submission status in DB
+            const submissionRepo = new SubmissionRepository();
+            const res = await submissionRepo.updateSubmission(payload);
+            console.log("Submission update response====>", res);
+
+            // Sending payload to another service
+            const response = await axios.post(`${SOCKET_SERVICE_URL}/sendPayload`, {
+                userId: payload.userId,
+                payload: payload.data
+            });
+            console.log("Payload sent:", response?.data);
+
+
+
+
+
+            return reply.status(200).send({
+                status: 'success',
+                message: 'Request was evaluated successfully',
+            });
+        }
+        catch (error) {
+            return reply.status(500).send({
+                status: 'error',
+                message: 'An error occurred while evaluating the request',
+            });
+
+        }
+    })
+
+    await fastify.register(require("./routes/api/apiRoutes"), { prefix: '/api' });
 
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const { Queue } = require('bullmq');
 const app = require('./app');
 const connectToDB = require('./config/dbConfig');
 const { PORT } = require('./config/serverConfig');
 const evaluationWorker = require('./workers/evaluationWorker');
+const redisConnection = require('./config/redisConfig');
 
 const fastify = require('fastify')({logger: false});
 
@@ -16,6 +18,7 @@ fastify.listen({ port: PORT, host: '0.0.0.0' },async (err) => {
     await connectToDB();
     console.log(`💚 Submission service started at ${PORT}`);
 
-    evaluationWorker('EvaluationQueue');
+    //evaluationWorker('EvaluationQueue');
+    
 })
   
