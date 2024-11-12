c3ed15483cd14ffd581a23c6563a2753116cfca1
parthmern <parthmern@gmail.com>
2024-11-11 23:14:27 -0500

    prod: bug fix3

--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -6,28 +6,27 @@ const SubmissionRepository = require('../repositories/submissionRepository');
 const { SOCKET_SERVICE_URL } = require('../config/serverConfig');
 
 function evaluationWorker(queue) {
+    console.log('Worker initialized');
     new Worker('EvaluationQueue', async job => {
         console.log("EvaluationQueue ==> job.name ==>", job.name);
         if (job.name === 'EvaluationJob') {
 
-            try {
+            console.log("EvaluationQueue ==> job.name ==>", job.name);
 
-                // updating submission status in DB
+            // updating submission status in DB
 
-                const submissionRepo = new SubmissionRepository();
-                const res = await submissionRepo.updateSubmission(job.data);
-                console.log("res====>", res);
+            const submissionRepo = new SubmissionRepository();
+            const res = await submissionRepo.updateSubmission(job.data);
+            console.log("res====>", res);
 
-                const response = await axios.post(`${SOCKET_SERVICE_URL}/sendPayload`, {
-                    userId: job.data.userId,
-                    payload: job.data
-                })
-                console.log("sending payload ...");
-                console.log(response?.data);
-                console.log(job.data);
-            } catch(error) {
-                console.log(error)
-            }
+            const response = await axios.post(`${SOCKET_SERVICE_URL}/sendPayload`, {
+                userId: job.data.userId,
+                payload: job.data
+            })
+            console.log("sending payload ...");
+            console.log(response?.data);
+            console.log(job.data);
+
         }
     }, {
         connection: redisConnection
