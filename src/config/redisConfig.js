9bb307c5667f417c1006612bc27dd987c5b4bd91
parthmern <parthmern@gmail.com>
2024-11-11 23:55:41 -0500

    prod: bug fix6

--- src/config/redisConfig.js
+++ src/config/redisConfig.js
@@ -3,10 +3,17 @@ const Redis = require('ioredis');
 const ServerConfig = require('./serverConfig');
 
 const redisConfig = {
-    port:ServerConfig.REDIS_PORT,
+    port: ServerConfig.REDIS_PORT,
     host: ServerConfig.REDIS_HOST,
     maxRetriesPerRequest: null,
-    lazyConnect: true
+    reconnectOnError: (error) => {
+        console.log('Redis reconnecting due to error:', error);
+        return true; // Automatically reconnect on error
+    },
+    maxRetriesPerRequest: null,  // Disable max retry limit
+    retryStrategy: (times) => {
+        return Math.min(times * 50, 2000); // Exponential backoff strategy
+    }
 };
 
 let redisConnection;
--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -7,30 +7,36 @@ const { SOCKET_SERVICE_URL } = require('../config/serverConfig');
 
 function evaluationWorker(queue) {
     console.log('Worker initialized');
+    
     new Worker('EvaluationQueue', async job => {
-        console.log("EvaluationQueue ==> job.name ==>", job.name);
-        if (job.name === 'EvaluationJob') {
 
+        try {
             console.log("EvaluationQueue ==> job.name ==>", job.name);
+            if (job.name === 'EvaluationJob') {
 
-            // updating submission status in DB
+                console.log("EvaluationQueue ==> job.name ==>", job.name);
 
-            const submissionRepo = new SubmissionRepository();
-            const res = await submissionRepo.updateSubmission(job.data);
-            console.log("res====>", res);
+                // updating submission status in DB
 
-            const response = await axios.post(`${SOCKET_SERVICE_URL}/sendPayload`, {
-                userId: job.data.userId,
-                payload: job.data
-            })
-            console.log("sending payload ...");
-            console.log(response?.data);
-            console.log(job.data);
+                const submissionRepo = new SubmissionRepository();
+                const res = await submissionRepo.updateSubmission(job.data);
+                console.log("res====>", res);
 
+                const response = await axios.post(`${SOCKET_SERVICE_URL}/sendPayload`, {
+                    userId: job.data.userId,
+                    payload: job.data
+                })
+                console.log("sending payload ...");
+                console.log(response?.data);
+                console.log(job.data);
+
+            }
+        }catch(error){
+            console.error("Worker encountered an error:", error);
         }
+
     }, {
-        connection: redisConnection,
-        concurrency: 5 
+        connection: redisConnection
     });
 }
 
